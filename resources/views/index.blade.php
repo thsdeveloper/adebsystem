73ebe67ade4e18a9a870d96719cc18efb763cac9
Thiago Pereira <ths.pereira@gmail.com>
2020-09-30 20:12:01 -0300

    [fix] mix node npm (2)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,24 @@
 @php
-$config = [
-'appName' => config('app.name'),
-'locale' => $locale = app()->getLocale(),
-'locales' => config('app.locales'),
-'githubAuth' => config('services.github.client_id'),
-];
+  $config = [
+      'appName' => config('app.name'),
+      'locale' => $locale = app()->getLocale(),
+      'locales' => config('app.locales'),
+      'githubAuth' => config('services.github.client_id'),
+  ];
 
-$polyfills = [
-'Promise',
-'Object.assign',
-'Object.values',
-'Array.prototype.find',
-'Array.prototype.findIndex',
-'Array.prototype.includes',
-'String.prototype.includes',
-'String.prototype.startsWith',
-'String.prototype.endsWith',
-];
+  $polyfills = [
+      'Promise',
+      'Object.assign',
+      'Object.values',
+      'Array.prototype.find',
+      'Array.prototype.findIndex',
+      'Array.prototype.includes',
+      'String.prototype.includes',
+      'String.prototype.startsWith',
+      'String.prototype.endsWith',
+  ];
 @endphp
-<!DOCTYPE html>
+  <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
   <meta charset="utf-8">
@@ -30,18 +30,12 @@ $polyfills = [
 </head>
 <body>
 <div id="app"></div>
-<script>window.config = @json($config)</script>
+<script>window.config = @json($config);</script>
 
 
 <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features={{ implode(',', $polyfills) }}"></script>
 
 
-@if (app()->isLocal())
 <script src="{{ mix('js/app.js') }}"></script>
-@else
-<script src="{{ mix('js/manifest.js') }}"></script>
-<script src="{{ mix('js/vendor.js') }}"></script>
-<script src="{{ mix('js/app.js') }}"></script>
-@endif
 </body>
 </html>
