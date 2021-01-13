1de19f4ebfb7e487117b00ece5e56ad20e6675c8
Thiago Pereira <ths.pereira@gmail.com>
2021-01-13 17:59:19 -0300

    Configuração de banco de dados

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$url = parse_url(getenv("DATABASE_URL"));
 return [
 
     /*
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $url["host"]),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', substr($url["path"], 1)),
+            'username' => env('DB_USERNAME', $url["user"]),
+            'password' => env('DB_PASSWORD', $url["pass"]),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => env('DB_SCHEMA', 'public'),
@@ -116,5 +116,4 @@ return [
         ],
 
     ],
-
 ];
