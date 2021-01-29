7e3b2a75ea898753511ffde62172f4cd5bcf2198
Thiago Pereira <ths.pereira@gmail.com>
2021-01-29 18:17:39 -0300

    integração jasperPHP

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -44,8 +44,8 @@ class ReportController extends Controller
         Log::info($output);
 
         //Compila o arquivo jrxml
-        $compile = JasperPHP::compile(public_path() . '/reports/visitantes.jrxml')->execute();
-        Log::info($compile);
+//        $compile = JasperPHP::compile(public_path() . '/reports/visitantes.jrxml')->execute();
+//        Log::info($compile);
 
         //Executa o arquivo de relatorio
         $process = JasperPHP::process(public_path() . '/reports/visitantes.jasper', $output, array($extencao), array('DescricaoNome' => 'Thiago'), $this->getDatabaseConfig())->execute();
