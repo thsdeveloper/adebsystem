4b1ddbdede41d912cb78e406dab295ca00b9fc0c
Thiago Pereira <ths.pereira@gmail.com>
2021-02-12 20:37:22 -0300

    Atualização do login vuejs

--- resources/js/pages/financeiro/home.vue
+++ resources/js/pages/financeiro/home.vue
@@ -1,61 +1,54 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <v-alert color="blue" type="success">Aguarde! Estamos trabalhando para oferecer recursos incríveis...</v-alert>
-      </v-col>
-    </v-row>
-  </v-container>
-<!--    <v-container>-->
-<!--        <v-row>-->
-<!--            <v-col cols="4">-->
-<!--                <v-card class="mx-auto" dark color="#26c6da" shaped hover :to="{ name: 'secretaria.visitantes'}">-->
-<!--                    <v-card-title>-->
-<!--                        <v-icon large left>mdi-bank</v-icon>-->
-<!--                        R$ 325.88 / Saldo Atual-->
-<!--                    </v-card-title>-->
-<!--                    <v-card-subtitle>Tenha o controle geral dos visitantes com relatórios sempre atualizados.-->
-<!--                    </v-card-subtitle>-->
-<!--                </v-card>-->
-<!--            </v-col>-->
-<!--            <v-col cols="4">-->
-<!--                <v-card class="mx-auto" dark color="#4caf50" shaped hover :to="{ name: 'secretaria.cartaRecomendacao'}">-->
-<!--                    <v-card-title>-->
-<!--                        <v-icon large left>mdi-trending-up</v-icon>-->
-<!--                        R$ 8.947,55 / Receita-->
-<!--                    </v-card-title>-->
-<!--                    <v-card-subtitle>Emissão de cartas, controle e relatórios com notificações e assinaturas.-->
-<!--                    </v-card-subtitle>-->
-<!--                </v-card>-->
-<!--            </v-col>-->
-<!--            <v-col cols="4">-->
-<!--                <v-card class="mx-auto" dark color="#f44336" shaped hover :to="{ name: 'members.all'}">-->
-<!--                    <v-card-title>-->
-<!--                        <v-icon large left>mdi-trending-down</v-icon>-->
-<!--                        R$ 18.947,55 / Despesas-->
-<!--                    </v-card-title>-->
-<!--                    <v-card-subtitle>Visualize, cadastre e gere relatorios completos sobre os membros.</v-card-subtitle>-->
-<!--                </v-card>-->
-<!--            </v-col>-->
-<!--        </v-row>-->
-<!--        <v-row>-->
-<!--            <v-col>-->
-<!--                <v-data-table-->
-<!--                        v-model="selected"-->
-<!--                        :headers="headers"-->
-<!--                        :items="desserts"-->
-<!--                        :single-select="singleSelect"-->
-<!--                        item-key="name"-->
-<!--                        show-select-->
-<!--                        class="elevation-1"-->
-<!--                >-->
-<!--                    <template v-slot:top>-->
-<!--                        <v-switch v-model="singleSelect" label="Single select" class="pa-3"></v-switch>-->
-<!--                    </template>-->
-<!--                </v-data-table>-->
-<!--            </v-col>-->
-<!--        </v-row>-->
-<!--    </v-container>-->
+    <v-container fluid>
+        <v-row>
+            <v-col cols="4">
+                <v-card class="mx-auto" dark color="#26c6da" shaped hover>
+                    <v-card-title>
+                        <v-icon large left>mdi-bank</v-icon>
+                        R$ 325.88 / Saldo Atual
+                    </v-card-title>
+                    <v-card-subtitle>Tenha o controle geral dos visitantes com relatórios sempre atualizados.
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+            <v-col cols="4">
+                <v-card class="mx-auto" dark color="#4caf50" shaped hover>
+                    <v-card-title>
+                        <v-icon large left>mdi-trending-up</v-icon>
+                        R$ 8.947,55 / Receita
+                    </v-card-title>
+                    <v-card-subtitle>Emissão de cartas, controle e relatórios com notificações e assinaturas.
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+            <v-col cols="4">
+                <v-card class="mx-auto" dark color="#f44336" shaped hover>
+                    <v-card-title>
+                        <v-icon large left>mdi-trending-down</v-icon>
+                        R$ 18.947,55 / Despesas
+                    </v-card-title>
+                    <v-card-subtitle>Visualize, cadastre e gere relatorios completos sobre os membros.</v-card-subtitle>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-data-table
+                        v-model="selected"
+                        :headers="headers"
+                        :items="desserts"
+                        :single-select="singleSelect"
+                        item-key="name"
+                        show-select
+                        class="elevation-1"
+                >
+                    <template v-slot:top>
+                        <v-switch v-model="singleSelect" label="Single select" class="pa-3"></v-switch>
+                    </template>
+                </v-data-table>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
   export default {
